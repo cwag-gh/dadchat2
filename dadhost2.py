19d42fd277e9486f6a9a2c36bf058a73f9367f9c
cwag-gh <54557911+cwag-gh@users.noreply.github.com>
2026-08-30 12:21:59 -0700

    Renames, added initial Room support, added named permission errors

--- dadhost2.py
+++ dadhost2.py
@@ -79,28 +79,94 @@ def truncate_to_encoded_length(s, n):
     return s
 
 
-def valid_username(s: str):
-    """Creates a valid username from s. Returns None if invalid."""
+class PermissionError(RuntimeError): pass
+
+
+def valid_username(s: str, existing_users: set[str]):
+    """Tries to create a valid username from s. Raises PermissionError with reason if unable."""
     s = s.strip()
     s = remove_chars(s, '/*#: \t\r\n')
     s = truncate_to_encoded_length(s, 14)
 
     if len(s) < 3:
-        return None
+        raise PermissionError('username too short (< 3 characters)')
     if s.lower() in {'private', 'general', 'dadserver'}:
-        return None
+        raise PermissionError('username a controlled term - not allowed')
     if not any(c.isalpha() for c in s):
-        return None
+        raise PermissionError('username must include at least one letter')
+    if s in existing_users:
+        raise PermissionError('username already taken')
 
     return s
 
 
-class Server:
+class Room:
+    def __init__(self, name: str, creator: str, public=False):
+        self.name: str = name
+        self.public: bool = public
+        self.creator: str = creator
+        self.admins: set[str] = {self.creator}
+        self.allowed: set[str] = set(self.admins) # Only used when public = False
+        self.banned: set[str] = set()             # Only used when public = True
+        self.present: set[str] = set(self.admins)
+
+    def kick(self, kicker: str, kickee: str):
+        if kicker not in self.admins:
+            raise PermissionError(f'{kicker} not an admin - not allowed to kick {kickee}')
+        self.present.remove(kickee)
+        if self.public:
+            self.banned.add(kickee)
+        else:
+            self.allowed.remove(kickee)
+
+    def invite(self, inviter: str, candidate: str):
+        if inviter not in self.admins:
+            raise PermissionError(f'{inviter} not an admin - not allowed to invite {candidate}')
+        if self.public:
+            if candidate in self.banned:
+                self.banned.remove(candidate)
+        else:
+            self.allowed.add(candidate)
+
+    def join(self, user: str):
+        if self.public:
+            if user in self.banned:
+                raise PermissionError(f'{user} has been banned from #{self.name}')
+        else:
+            if user not in self.allowed:
+                raise PermissionError(f'{user} does not have permission to join #{self.name}')
+        self.present.add(user)
+
+    def leave(self, user: str):
+        self.present.remove(user)
+
+    def promote(self, promoter: str, candidate: str):
+        if promoter != self.creator:
+            raise PermissionError(f'Only the room creator has permission to promote')
+        if not self.public and candidate not in self.allowed:
+            raise PermissionError(
+                f'{candidate} must first be allowed into #{self.name} before promotion to admin')
+        self.admins.add(candidate)
+
+
+async def close_connection(writer: asyncio.StreamWriter) -> None:
+    """Close a socket that may be broken."""
+    # Safe way to close a socket no matter the socket state
+    writer.close()
+    try:
+        await asyncio.wait_for(writer.wait_closed(), CLOSE_TIMEOUT)
+    except (OSError, asyncio.TimeoutError):
+        pass
+
+
+class ChatServer:
     """Keep track of connected users and rooms."""
+
     def __init__(self):
         self.users = set() # Set of users
+        self.rooms = dict() # map of room name to Room
 
-    async def handle(self, reader, writer):
+    async def handle_client(self, reader, writer):
         parser = MessageParser()
         username = None
         addr = writer.get_extra_info('peername')
@@ -118,9 +184,10 @@ class Server:
                 if username is None:
                     # First message is the username
                     print(f'Checking if {msg} is a valid username')
-                    username = valid_username(msg)
-                    if username is None:
-                        send('PRIVATE *DadServer*: Invalid username')
+                    try:
+                        username = valid_username(msg)
+                    except PermissionError as e:
+                        send(f'PRIVATE *DadServer*: {str(e)}')
                         break
                     print(f'{addr} is now user {username}')
                     self.users.add(username)
@@ -143,15 +210,15 @@ class Server:
 
 
 async def main():
-    server = Server()
-    server_stream = await asyncio.start_server(
-        server.handle, '0.0.0.0', 12347)
+    chat = ChatServer()
+    server = await asyncio.start_server(
+        chat.handle_client, '0.0.0.0', 12347)
 
-    addrs = ', '.join(str(sock.getsockname()) for sock in server_stream.sockets)
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
     print(f'Serving on {addrs}')
 
-    async with server_stream:
-        await server_stream.serve_forever()
+    async with server:
+        await server.serve_forever()
 
 
 asyncio.run(main())
